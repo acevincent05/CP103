f189c9cbea3dbe31e11ab84bc3c435582a01a982
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-11 02:20:25 +0800

    #

--- Activity 2/Insert.cpp
+++ Activity 2/Insert.cpp
@@ -5,8 +5,14 @@ using namespace std;
 int main(){
 
     int arr[5] = {3, 6, 12, 15};
+	int positionAdd = 2;
+	int addValue = 9;
 
+	for(int i=5, i>positionAdd; i--){
+		arr[i] = arr[i-1];
+	}
 	
-	
+	arr[positionAdd] = addValue;
+
     return 0;
 }
