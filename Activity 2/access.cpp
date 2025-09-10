346114629f6a0b2f47a28e25038b81ef0cc3f12c
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-11 01:25:02 +0800

    #

--- Activity 2/access.cpp
+++ Activity 2/access.cpp
@@ -6,6 +6,15 @@ int main() {
 	
 	int arr[4] = {3, 6, 9, 12};
 	
-	cout << arr[0];
+	cout << "Array: ";
+
+	for(int i=0; i<4; i++){
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n" << endl;
+	cout << arr[0] << endl;
+
+	
 	return 0;
 }
