4985acc3f77763888944c782060a494747a9acfa
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-11 03:51:49 +0800

    #

--- Activity 2/Cantenate.cpp
+++ Activity 2/Cantenate.cpp
@@ -3,41 +3,45 @@
 using namespace std;
 
 int main() {
-	
-	int maxA = 10; 
-    int maxB = 10; 
     int maxResult = 20; 
+	int maxA = 10; 
+	int na = 4;
     int lista[maxA] = {1, 2, 3, 4}; 
+	int maxB = 10;
+	int nb = 3;  
     int listb[maxB] = {5, 6, 7}; 
-    int na = 4; 
-    int nb = 3; 
     int result[maxResult]; 
     int resultSize = 0; 
 
-	cout << "Array: ";
+	cout << "List A: ";
 
-	for(int i = 0 ; i < currentSize; i++){
-		cout << arr[i] << " ";
+	for(int i = 0 ; i < na; i++){
+		cout << lista[i] << " ";
 	}
 
-    // Check if result array can hold all elements
-    if (na + nb <= MAX_SIZE_RESULT) {
-        // Copy elements from lista
+	cout << "\n";
+	
+	cout << "List B: ";
+
+	for(int i = 0 ; i < nb; i++){
+		cout << listb[i] << " ";
+	}
+
         for (int i = 0; i < na; i++) {
             result[i] = lista[i];
             resultSize++;
         }
-        // Append elements from listb
+        
         for (int i = 0; i < nb; i++) {
             result[resultSize] = listb[i];
             resultSize++;
         }
 
 	cout << "\n";
-    cout << "New array: ";
+    cout << "Result: ";
 
-	for(int i = 0 ; i < currentSize; i++){
-		cout << arr[i] << " ";
+	for(int i = 0 ; i < resultSize; i++){
+		cout << result[i] << " ";
 	}
 
 	return 0;
