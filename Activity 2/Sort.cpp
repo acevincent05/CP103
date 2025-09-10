f14d53b7caa9e64268af71a50d94884df912e414
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-11 03:34:02 +0800

    #

--- Activity 2/Sort.cpp
+++ Activity 2/Sort.cpp
@@ -6,7 +6,7 @@ int main() {
 	
 	int max = 6;
 	int currentSize = 5;
-	int arr[max] = {3, 6, 9, 12, 15};
+	int arr[max] = {9, 3, 12, 15, 6};
 	
 
 	cout << "Array: ";
@@ -18,14 +18,16 @@ int main() {
 	for (int i = 0; i < currentSize - 1; i++) {
         for (int j = 0; j < currentSize - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap elements
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
-	
+
+	cout << "\n";
+    cout << "New array: ";
+
 	for(int i = 0 ; i < currentSize; i++){
 		cout << arr[i] << " ";
 	}
