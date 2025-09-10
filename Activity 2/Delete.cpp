551045426b3c44e685c1d2e318deffbf19b61ad4
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-11 02:46:07 +0800

    #

--- Activity 2/Delete.cpp
+++ Activity 2/Delete.cpp
@@ -4,27 +4,29 @@ using namespace std;
 
 int main(){
 
-    int arr[5] = {3, 6, 9, 12};
+	int max = 5;
+	int currentSize = 4;
+    int arr[max] = {3, 6, 9, 12};
 	
 
 	cout << "Old array: ";
 
-	for(int i=0; i<4; i++){
+	for(int i=0; i<currentSize; i++){
 		cout << arr[i] << " ";
 	}
 
 	int positionDelete = 2;
 
-	for(int i=positionDelete; i<4-1; i++){
+	for(int i=positionDelete; i<currentSize-1; i++){
 		arr[i] = arr[i+1];
 	}
 
-	
+	currentSize--;
 
 	cout << "\n";
     cout << "New array: ";
 
-	for(int i=0; i<4; i++){
+	for(int i=0; i<currentSize; i++){
 		cout << arr[i] << " ";
 	}
 
