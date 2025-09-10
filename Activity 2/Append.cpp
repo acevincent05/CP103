cd1a5e9925791d8d7b24d35fbe31345b26cc4581
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-11 01:58:21 +0800

    #

--- Activity 2/Append.cpp
+++ Activity 2/Append.cpp
@@ -4,14 +4,27 @@ using namespace std;
 
 int main(){
 
-	int max = 10;
+	int max = 6;
 	int currentSize = 5;
 
     int arr[max] = {3, 6, 9, 12, 15};
 
+	cout << "Old array: ";
+
+	for(int i = 0; i <= (currentSize-1); i++) {
+		cout << arr[i] << " ";
+	}
+
 	if(currentSize < max){
 		arr[currentSize] = 18;
 		currentSize++;
+	}
+
+	cout << "\n";
+	cout << "New array: ";
+
+	for(int i = 0; i <= (currentSize-1); i++) {
+		cout << arr[i] << " ";
 	}
 
     return 0;
