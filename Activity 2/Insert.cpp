7604ae8909fcdc2aec5cd726438f31a440603b29
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-11 02:22:11 +0800

    #

--- Activity 2/Insert.cpp
+++ Activity 2/Insert.cpp
@@ -8,11 +8,24 @@ int main(){
 	int positionAdd = 2;
 	int addValue = 9;
 
+	cout << "Old array: ";
+
+	for(int i=0; i>3; i++){
+		cout << arr[i] << " ";
+	}
+
 	for(int i=5; i>positionAdd; i--){
 		arr[i] = arr[i-1];
 	}
 	
 	arr[positionAdd] = addValue;
+
+	cout << "\n" << endl;
+    cout << "New array: ";
+
+	for(int i=0; i<4; i++){
+		cout << arr[i] << " ";
+	}
 
     return 0;
 }
