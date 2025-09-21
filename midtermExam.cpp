6bc468da7f57e0638de9bc63566f7fa4ca765f74
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-22 00:51:41 +0800

    #

--- midtermExam.cpp
+++ midtermExam.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib> // for clearing screen
 
 using namespace std;
 
@@ -7,12 +8,14 @@ string* movie_list;  // pointer for dynamic array
 int list_size;
 
 void showAll() {
+    system("cls"); // clears screen
     for(int i = 0; i <= list_size - 1; i++){
         cout << i+1 << ". " << movie_list[i] << endl;
     }
 }
 
 int main(){
+    system("cls");
     cout << "Enter Movie list size: ";
     cin >> list_size;
     cin.ignore(); 
