18f4a0ac0c59d77b0d9f0505a7fde4a44a05965b
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-22 00:14:02 +0800

    #

--- midtermExam.cpp
+++ midtermExam.cpp
@@ -5,13 +5,16 @@ using namespace std;
 
 int main(){
     int list_size; 
-    string movie_list[list_size] = {};
-
     cout << "Enter Movie list size: ";
     cin >> list_size;
 
+    string movie_list[list_size] = {};
+    
     for(int i = 0; i <= list_size - 1; i++){
         cout << "Enter Movie title: ";
         cin >> movie_list[i];
     }
+
+    
+
 }
