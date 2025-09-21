50e11165bdab1e1fd16aa4cfe821fdbdcae34bfb
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-22 00:35:12 +0800

    #

--- midtermExam.cpp
+++ midtermExam.cpp
@@ -3,24 +3,28 @@
 
 using namespace std;
 
-int list_size; 
-string movie_list[0] = {};
-
-int main(){
-    
-    cout << "Enter Movie list size: ";
-    cin >> list_size;
-    
-    for(int i = 0; i <= list_size - 1; i++){
-        cout << "Enter Movie title: ";
-        cin >> movie_list[i];
-    }
-    
-    return 0;
-}
+string* movie_list;  // pointer for dynamic array
+int list_size;
 
 void showAll() {
     for(int i = 0; i <= list_size - 1; i++){
         cout << i+1 << ". " << movie_list[i] << endl;
     }
 }
+
+int main(){
+    cout << "Enter Movie list size: ";
+    cin >> list_size;
+    
+    movie_list = new string[list_size];
+    
+    for(int i = 0; i <= list_size - 1; i++){
+        cout << "Enter Movie title: ";
+        cin >> movie_list[i];
+    }
+    
+    showAll();
+
+    return 0;
+}
+
