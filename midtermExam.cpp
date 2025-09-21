d8812ddf7619bea5c2cc92304a5a02bff31e85a0
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-22 00:19:54 +0800

    #

--- midtermExam.cpp
+++ midtermExam.cpp
@@ -14,9 +14,12 @@ int main(){
         cout << "Enter Movie title: ";
         cin >> movie_list[i];
     }
-
-    for(int i = 0; i <= list_size - 1; i++){
-        cout << i+1 << ". " << movie_list[i] << endl;
+    
+    void showAll() {
+        for(int i = 0; i <= list_size - 1; i++){
+            cout << i+1 << ". " << movie_list[i] << endl;
     }
+    }
+    
 
 }
