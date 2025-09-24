a501d7fe7e35eae6deff7537870f51d579a10699
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-24 15:24:09 +0800

    #

--- practice_c++/classes/classes2.cpp
+++ practice_c++/classes/classes2.cpp
@@ -21,5 +21,15 @@ class House{
 };
 
 int main(){
+    House house1;
+
+    house1.floors = 3;
+    house1.paint = "white";
+    house1.doors = 10;
+    house1.windows = 8;
+
+    std::cout << house1.floors << std::endl;
+
+
     return 0;
 }
