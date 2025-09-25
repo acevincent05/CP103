0891d5bee0cb3c443e8367203634f3f78c1fec10
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-25 19:23:51 +0800

    #

--- practice_c++/pointers/pointers.cpp
+++ practice_c++/pointers/pointers.cpp
@@ -2,9 +2,14 @@
 
 int main(){
     std::string name = "bro";
+    int num = 1;
 
     std::string *pName = &name;
+    int *pNum = &num;
 
     std::cout<< pName << std::endl;
-    std::cout<< *pName;
+    std::cout<< *pName << std::endl;
+    std::cout<< pNum << std::endl;
+    std::cout<< *pNum << std::endl;
+    std::cout<< *pNum+10 << std::endl;
 }
