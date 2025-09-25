d244a03a2d75d11964b13a55a249694b4e149fde
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-25 11:35:51 +0800

    #

--- Coursera/ccp-memory/foo.cpp
+++ Coursera/ccp-memory/foo.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+void foo()
+{
+    int y = 27;
+
+    std::cout << "y in foo(): " << y << std::endl;
+    std::cout << "&y in foo(): " << &y << std::endl;
+}
+
 int main(){
     int x = 10;
     
     std::cout<< "num in main(): " << x << std::endl;
     std::cout<< "&num in main(): " << &x << std::endl;
+
+    foo();
+
 }
