de8ff808772f4704962a1218179d2792b4d5448b
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-25 19:30:00 +0800

    #

--- practice_c++/pointers/pointers.cpp
+++ practice_c++/pointers/pointers.cpp
@@ -3,13 +3,17 @@
 int main(){
     std::string name = "bro";
     int num = 1;
+    std::string names[5] = {"Ace", "Bea", "Loreyn", "Shyne", "Flo"};
 
     std::string *pName = &name;
     int *pNum = &num;
+    std::string *arrNames = names;
 
     std::cout<< pName << std::endl;
     std::cout<< *pName << std::endl;
     std::cout<< pNum << std::endl;
     std::cout<< *pNum << std::endl;
     std::cout<< *pNum+10 << std::endl;
+    std::cout<< arrNames << std::endl;
+    std::cout<< *arrNames << std::endl;
 }
