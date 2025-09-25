490fda496bb1d6abb78d026602b8e01d049d99e6
Ace Gamitin <gamitinacevincent@gmail.com>
2025-09-25 11:24:01 +0800

    #

--- Coursera/addressOf.cpp
+++ Coursera/addressOf.cpp
@@ -3,11 +3,14 @@
 int main() {
     int x = 1;
     int y = 2;
+    std::string z = "hi";
 
     std::cout << "Value: " << x << std::endl;
     std::cout << "Address: " << &x << x << std::endl;
     std::cout << "Value: " << y << std::endl;
-    std::cout << "Address: " << &y << x;
+    std::cout << "Address: " << &y << x << std::endl;
+    std::cout << "Value: " << z << std::endl;
+    std::cout << "Address: " << &z;
 
     return 0;
 }
