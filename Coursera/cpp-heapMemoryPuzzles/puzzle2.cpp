0214c64fe39fcac060e932ed73f72d975bd4706d
Ace Gamitin <gamitinacevincent@gmail.com>
2025-10-01 09:49:20 +0800

    #puzzle2

---
+++ Coursera/cpp-heapMemoryPuzzles/puzzle2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int *x = new int;
+    int &y = *x;
+    y = 4;
+
+    cout << &x << endl;
+    cout << x << endl;
+    cout << *x << endl;
+    
+    cout << &y << endl;
+    cout << y << endl;
+    cout << *y << endl;
+
+    return 0;
+}
